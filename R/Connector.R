1cde698e7c6080a95419621012cd8a9533a966a0
VictorPlaysBadminton <162258396+VictorPlaysBadminton@users.noreply.github.com>
2026-01-07 15:07:47 +1300

    basic function of get_datasource

--- R/Connector.R
+++ R/Connector.R
@@ -6,7 +6,7 @@
 #'
 #' @examples
 #' Connector$new(token = yourToken)
-#' 
+#'
 #' @import R6
 #' @import tidyverse
 #' @import httr2
@@ -16,7 +16,7 @@
 
 Connector <- R6::R6Class(
   "Connector",
-  
+
   private = list(
     token = NULL,
     service = NULL,
@@ -27,9 +27,9 @@ Connector <- R6::R6Class(
     host = NULL,
     user = NULL
   ),
-  
+
   public = list(
-    
+
     # -------- constructor --------
     initialize = function(
     token   = Sys.getenv("DATAMESH_TOKEN"),
@@ -42,16 +42,16 @@ Connector <- R6::R6Class(
           call. = FALSE
         )
       }
-      
+
       private$token   <- token
       private$service <- service
       private$verify  <- verify
-      
+
       # parse URL
       parsed <- httr2::url_parse(private$service)
       private$proto <- parsed$scheme
       private$host  <- parsed$hostname
-      
+
       # auth headers
       if (startsWith(token, "Bearer ")) {
         private$auth_headers <- c(
@@ -63,31 +63,31 @@ Connector <- R6::R6Class(
           "X-DATAMESH-TOKEN"  = token
         )
       }
-      
+
       private$gateway <- private$service
-      
+
       # setup session
       private$user <- private$session
     },
-    
+
     # -------- simple accessors --------
     get_host = function() {
       private$host
     },
-    
+
     check_info = function() {
       NA_real_
     },
-    
+
     # -------- status --------
     status = function() {
       req <- request(private$gateway) |>
         req_headers(!!!private$auth_headers) |>
         req_method("GET") |>
         req_options(ssl_verifypeer = private$verify)
-      
+
       resp <- req_perform(req)
-      
+
       if (resp_status(resp) == 200) {
         message(sprintf(
           "Datamesh connector created for %s",
@@ -98,7 +98,7 @@ Connector <- R6::R6Class(
         FALSE
       }
     },
-    
+
     # -------- catalog --------
     get_catalog = function(
     search     = "",
@@ -106,16 +106,16 @@ Connector <- R6::R6Class(
     geofilter  = NULL,
     limit      = NA_integer_
     ) {
-      
+
       query <- list()
       if (nzchar(search)) query$search <- search
       if (!is.na(limit))  query$limit  <- limit
-      
+
       if (!is.null(timefilter)) {
         in_trange <- private$format_timefilter(timefilter)
         if (nzchar(in_trange)) query$in_trange <- in_trange
       }
-      
+
       if (!is.null(geofilter)) {
         if (is.character(geofilter)) {
           query$geom_intersects <- geofilter
@@ -125,65 +125,65 @@ Connector <- R6::R6Class(
           stop("Invalid geofilter")
         }
       }
-      
+
       req <- httr2::request(
         paste0(private$proto, "://", private$host, "/datasource/")
       ) |>
         httr2::req_headers(!!!private$auth_headers) |>
         httr2::req_url_query(!!!query) |>
         httr2::req_method("GET")
-      
+
       resp <- httr2::req_perform(req)
-      
+
       if (httr2::resp_status(resp) != 200) {
         stop("Failed to retrieve catalog")
       }
-      
+
       content <- httr2::resp_body_json(resp, simplifyVector = TRUE)
       return(Catalog$new(content, self))
     },
-    
+
     # -------- datasource metadata --------
     get_datasource = function(datasource_id) {
       req <- httr2::request(
         paste0(private$proto, "://", private$host, "/datasource/", datasource_id)
       ) |>
         httr2::req_headers(!!!private$auth_headers)
-      
+
       resp <- httr2::req_perform(req)
-      
+
       status <- httr2::resp_status(resp)
       if (status == 404) stop("Datasource not found")
       if (status == 401) stop("Unauthorized")
       if (status != 200) stop("Request failed")
-      
+
       data <- httr2::resp_body_json(resp, simplifyVector = TRUE)
-      
+
       props <- data$properties
       props$id   <- datasource_id
       props$geom <- data$geometry
       props$bbox <- data$bbox
-      
+
       Datasource$new(props)
     },
-    
+
     # -------- load datasource --------
     load_datasource = function(
     datasource_id,
     size_limit = 1e9,
     row_limit  = 2e6
     ) {
-      
+
       #stage <- private$(list(datasource = datasource_id))
-      
+
       if (stage$size > size_limit) {
         stop("Datasource too large for memory")
       }
-      
+
       if (stage$dlen > row_limit) {
         warning("Row limit exceeded")
       }
-      
+
       req <- httr2::request(
         paste0(private$gateway, "/data/", datasource_id)
       ) |>
@@ -191,33 +191,33 @@ Connector <- R6::R6Class(
           !!!private$auth_headers,
           Accept = "application/json"
         )
-      
+
       resp <- httr2::req_perform(req)
-      
+
       if (httr2::resp_status(resp) != 200) {
         stop("Failed to load datasource")
       }
-      
+
       httr2::resp_body_json(resp, simplifyVector = TRUE)
     },
-    
+
     # -------- query --------
     query = function(
     query_input,
     query_size_limit = 1e9,
     row_limit = 2e6
     ) {
-      
+
       #stage <- private$(query_input)
-      
+
       if (is.null(stage)) stop("No data returned")
-      
+
       if (stage$size > query_size_limit)
         stop("Query size limit exceeded")
-      
+
       if (stage$dlen > row_limit)
         warning("Row limit exceeded")
-      
+
       req <- httr2::request(paste0(private$service, "/oceanql/")) |>
         httr2::req_headers(
           !!!private$user$add_header(private$auth_headers),
@@ -225,9 +225,9 @@ Connector <- R6::R6Class(
           Accept = "application/json"
         ) |>
         httr2::req_body_json(query_input)
-      
+
       resp <- httr2::req_perform(req)
-      
+
       httr2::resp_body_json(resp, simplifyVector = TRUE)
     }
   )
--- R/Datasource.R
+++ R/Datasource.R
@@ -2,11 +2,11 @@
 #'
 #' Objected used to hold and organise datasource information.
 #'
-#' @param data 
+#' @param data
 #'
 #' @examples
 #' Datasource$new(data)
-#' 
+#'
 #' @import R6
 #' @import tidyverse
 #' @import httr2
@@ -16,4 +16,119 @@
 
 Datasource <- R6::R6Class(
   "Datasource",
+  public = list(
+        coordinates = NULL,
+        created= NULL,
+        dataschema= NULL,
+        description= NULL,
+        details= NULL,
+        driver= NULL,
+        driver_args= NULL,
+        expires= NULL,
+        geom= NULL,
+        id= NULL,
+        info= NULL,
+        labels= NULL,
+        modified= NULL,
+        name= NULL,
+        parameters= NULL,
+        parchive= NULL,
+        pforecast= NULL,
+        tags= NULL,
+        tend= NULL,
+        tstart= NULL,
+        # ------ Properties ------
+        attributes= NULL,
+        bounds= NULL,
+        geometry= NULL,
+        variables= NULL,
+        initialize = function(props){
+          prop_names = names(props)
+          if("coordinates" %in% prop_names){
+            self$coordinates = props$coordinates
+          }
+
+          if("created" %in% prop_names){
+            self$created = props$created
+          }
+
+          if("dataschema" %in% prop_names){
+            self$dataschema = props$dataschema
+          }else if("schema" %in% prop_names){
+            self$dataschema = props$schema
+          }
+
+          if("description" %in% prop_names){
+            self$description = props$description
+          }
+
+          if("details" %in% prop_names){
+            self$details = props$details
+          }
+
+          if("driver" %in% prop_names){
+            self$driver = props$driver
+          }
+
+          if("driver_args" %in% prop_names){
+            self$driver_args = props$driver_args
+          }else if("args" %in% prop_names){
+            self$driver_args = props$args
+          }
+
+          if("expires" %in% prop_names){
+            self$expires = props$expires
+          }
+
+          if("geom" %in% prop_names){
+            self$geom = props$geom
+          }
+          if("id" %in% prop_names){
+            self$id = props$id
+          }
+          if("info" %in% prop_names){
+            self$info = props$info
+          }
+          if("labels" %in% prop_names){
+            self$labels = props$labels
+          }
+          if("modified" %in% prop_names){
+            self$modified = props$modified
+          }
+          if("name" %in% prop_names){
+            self$name = props$name
+          }
+          if("parameters" %in% prop_names){
+            self$parameters = props$parameters
+          }
+
+          if("parchive" %in% prop_names){
+            self$parchive = props$parchive
+          }
+
+          if("pforecast" %in% prop_names){
+            self$pforecast = props$pforecast
+          }
+          if("tags" %in% prop_names){
+            self$tags = props$tags
+          }
+          if("tend" %in% prop_names){
+            self$tend = props$tend
+          }
+          if("tstart" %in% prop_names){
+            self$tstart = props$tstart
+          }
+          if("metadata" %in% prop_names){
+            self$attributes = props$metadata
+          }
+          # TODO: Implement GeoJSON display of bounds??
+
+          if("bbox" %in% prop_names){
+            self$bounds = props$bbox
+          }
+          if("schema" %in% prop_names){
+            self$variables = props$schema$data_vars
+          }
+        }
+  )
 )
